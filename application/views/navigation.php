edd658709fef5d7379ebacebe38c1baa73db7d78
Mark Daniel Bandilla <markbandilla.dpwm@yahoo.com>
2023-02-12 03:58:12 +0800

    CI to mockup

--- application/views/navigation.php
+++
@@ -1,9 +0,0 @@
-<div id="header">
-    	<div class="main">   
-            <div class="menu">
-                <!--img src="<?=site_url()?>img/menu.png" /-->                                              
-                WEB DEVELOPMENT | SOFTWARE | OUTSOURCING | PORTFOLIO | ABOUT US | CONTACT US | GET A QUATE
-            </div><!-- menu -->
-       	</div><!-- main -->
-    </div><!-- header -->
-    
